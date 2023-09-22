99f346e74d877539a006107a90c0e4ef295c6ace
gem-tah <taylor.higgins@globalenergymonitor.org>
2023-09-22 15:12:32 -0500

    actually removing zeroes for all in correct graph 'additions' not 'status'

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ layout_chosen = '2 columns'  # options: '1 column', '2 columns'
 
 # filepath = 'https://github.com/GlobalEnergyMonitor/GCPT-dashboard/blob/main/data/GCPT%20dashboard%20data%202022-07%20-%20processed%20for%20Dash%202022-08-31_1036.xlsx?raw=true'
 # filepath = 'https://github.com/GlobalEnergyMonitor/GCPT-dashboard/blob/main/data/GCPT%20dashboard%20data%202023-02%20-%20processed%20for%20Dash%202023-02-13_1526.xlsx?raw=true'
-filepath = 'https://github.com/GlobalEnergyMonitor/GCPT-dashboard/blob/main/data/GCPT%20dashboard%20data%202023-08%20-%20processed%20for%20Dash%202023-09-07_1623.xlsx?raw=true'
+filepath = 'https://github.com/GlobalEnergyMonitor/GCPT-dashboard/blob/main/data/GCPT%20dashboard%20data%202023-08%20-%20processed%20for%20Dash%202023-09-22_1458.xlsx?raw=true'
 
 # # ===================================
 dash_data_xl = pd.ExcelFile(filepath, engine='openpyxl')
